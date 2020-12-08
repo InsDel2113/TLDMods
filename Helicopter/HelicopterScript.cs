3dcc104b2ecd7011b7613b6acfb65be9b0f60f8e
InsDel2113 <52561077+InsDel2113@users.noreply.github.com>
2020-12-08 07:10:01 -0700

    Initial Helicopter Commit

---
+++ Helicopter/HelicopterScript.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TLDLoader;
+using UnityEngine;
+
+namespace HelicopterMod
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(carscript))]
+    [RequireComponent(typeof(seatscript))]
+    class HelicopterScript : MonoBehaviour
+    {
+        public float HelicopterForce = 2800f;
+        public float HelicopterTorque = 200f;
+        public float HelicopterTurnSpeed = 350;
+        public int rotorSpinSpeed = 15;
+
+        public Rigidbody rb;
+        public carscript car;
+        public seatscript seat;
+        public AudioSource helisource;
+        public bool helicopterGrounded = true;
+        public bool initialized = false;
+
+        void Setup()
+        {
+            if ( gameObject.transform.GetChild(0).name != "SeatPos" )
+            {
+               QuickInit();
+               return;
+            }
+            initialized = true;
+            // get components, cached calls to save cpu time
+            rb = gameObject.GetComponent<Rigidbody>();
+            car = gameObject.GetComponent<carscript>();
+            seat = gameObject.GetComponent<seatscript>();
+
+            // rigidbody setup
+            rb.mass = 5;
+            rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+            // seat setup
+            seat.sitPos = gameObject.transform.GetChild(0);
+            seat.inUse = false;
+            seat.drivingActive = true;
+            seat.RB = rb;
+            seat.limitLessRot = true; // unlock camera in seat
+            car.driverSeat = seat;
+
+            // scaling and position
+            gameObject.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f); // just a bit bigger
+
+            // audio setup
+            helisource = gameObject.GetComponent<AudioSource>();
+        }
+
+        public void Start()
+        {
+            Setup();
+        }
+        public void DoEffects()
+        {
+            helisource.PlayOneShot(helisource.clip, 0.20f); // play sound
+            gameObject.transform.GetChild(1).transform.Rotate(0, rotorSpinSpeed, 0); // spin rotors
+        }
+        public void Update()
+        {
+            if (!initialized)
+                return;
+            if (Physics.Raycast(seat.transform.position, transform.TransformDirection(-Vector3.up), 1.0f))
+            {
+                helicopterGrounded = true;
+            }
+            else
+            {
+                helicopterGrounded = false;
+            }
+            if (rb.velocity.magnitude > 0.85f && !helicopterGrounded)
+            {
+                DoEffects();
+            }
+            else
+            {
+                if (!Input.GetKey(KeyCode.Space))
+                {
+                    helisource.Stop();
+                }
+            }
+        }
+        public void FixedUpdate()
+        {
+            if (!initialized)
+                return;
+            if (gameObject && seat.inUse)
+            {
+                DoMovement();
+            }
+        }
+        
+        public void OnGUI()
+        {
+
+        }
+
+        public GameObject QuickInit()
+        {
+            HelicopterMod newHeliMod = new HelicopterMod();
+            newHeliMod.OnLoad();
+            GameObject obj = UnityEngine.Object.Instantiate(newHeliMod.obj);
+            obj.AddComponent<HelicopterScript>();
+            return obj;
+        }
+
+        private void DoMovement()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.up * HelicopterForce * Time.deltaTime);
+                DoEffects(); // just in case the ground check is true but user still pressing space
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                gameObject.GetComponent<Rigidbody>().AddTorque(gameObject.transform.forward * -HelicopterTorque * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                gameObject.GetComponent<Rigidbody>().AddTorque(gameObject.transform.forward * HelicopterTorque * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                gameObject.GetComponent<Rigidbody>().AddTorque(gameObject.transform.right * -HelicopterTorque * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                gameObject.GetComponent<Rigidbody>().AddTorque(gameObject.transform.right * HelicopterTorque * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                gameObject.GetComponent<Rigidbody>().AddTorque(-gameObject.transform.up * HelicopterTurnSpeed * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.LeftAlt))
+            {
+                gameObject.GetComponent<Rigidbody>().AddTorque(gameObject.transform.up * HelicopterTurnSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
---
+++ Helicopter/Mod.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using TLDLoader;
+using UnityEngine;
+
+namespace HelicopterMod
+{
+    public class HelicopterMod : Mod
+    {
+        // The ID of the mod - Should be unique
+        public override string ID
+        {
+            get
+            {
+                return "Helicopter";
+            }
+        }
+
+        // The name of the mod that is displayed
+        public override string Name
+        {
+            get
+            {
+                return "HelicopterVehicle";
+            }
+        }
+
+        // The name of the author
+        public override string Author
+        {
+            get
+            {
+                return "InsDel2113";
+            }
+        }
+
+        // The version of the mod
+        public override string Version
+        {
+            get
+            {
+                return "1.1";
+            }
+        }
+
+        public override bool UseAssetsFolder
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        AssetBundle bundle = null;
+        public GameObject obj = null;
+        GameObject helicopterObj;
+
+        public override void OnLoad()
+        {
+            if (bundle == null || obj == null)
+            {
+                bundle = LoadAssets.LoadBundle(this, "ucak-main1.unity3d");
+                obj = bundle.LoadAsset("ucak-main1.prefab") as GameObject;
+            }
+            if (obj != null)
+            {
+                helicopterObj = UnityEngine.Object.Instantiate(obj);
+                HelicopterScript rb = helicopterObj.AddComponent<HelicopterScript>(); // add helicopter script
+                                                                                      // adds all the required stuff for it, just make sure prefab is setup correctly
+                                                                                      // TODO: MAKE HELICOPTER SCRIPT MORE DYNAMIC!
+                helicopterObj.transform.position = mainscript.M.player.transform.position + (Vector3.back * 5) + (Vector3.up * 5); // at the home, this is just outside the back door!
+                if (itemdatabase.d.gbike01.GetComponent<HelicopterScript>())
+                {
+                    Debug.Log("Helicopter script was already attached to bike - weird?");
+                    return;
+                }
+                itemdatabase.d.gbike01.AddComponent<HelicopterScript>();
+                // spawn with bike
+            }
+            else
+            {
+                Debug.LogError("Failed to load obj/ucak-main1.prefab from AssetBundles - HelicopterMod disabled");
+            }
+            if (bundle == null)
+            {
+                Debug.LogError("Failed to load bundle/ucak-main1.unity3d from AssetBundles - HelicopterMod disabled");
+            }
+        }
+    }
+}
